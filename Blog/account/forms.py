a353408a5cfd531a4b42b4229ec0abfba966bf6b
Nikita(WorkLaptopLin) <falgerok.master@gmail.com>
2021-06-08 22:30:13 +0300

    Поправил форму логина и регистрации, добавил им стили

--- Blog/account/forms.py
+++ Blog/account/forms.py
@@ -2,8 +2,15 @@ from django.contrib.auth.models import User
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(label="Имя пользователя", widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Логин',
+    }))
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Пароль',
+    }))
+
 
 
 class UserRegistrationForm(forms.ModelForm):
@@ -13,6 +20,34 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают.')
+        return cd['password2']
+
+class UserRegistrationForm2(forms.ModelForm):
+    username = forms.CharField(label='Имя пользователя:', widget=forms.TextInput(attrs={
+        'class': 'form-control',
+    }))
+    first_name = forms.CharField(label='Имя:', widget=forms.TextInput(attrs={
+        'class': 'form-control',
+    }))
+    email = forms.CharField(label='Электронная почта:', widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+    }))
+    password = forms.CharField(label='Пароль:', widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+    }))
+    password2 = forms.CharField(label='Повторите пароль:', widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+    }))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
 
     def clean_password2(self):
         cd = self.cleaned_data
--- Blog/account/views.py
+++ Blog/account/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserRegistrationForm2
 from django.contrib.auth.decorators import login_required
 
 def user_login(request):
@@ -32,15 +32,15 @@ def user_logout(request):
 
 def register(request):
     if request.method == "POST":
-        user_form = UserRegistrationForm(request.POST)
+        user_form = UserRegistrationForm2(request.POST)
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
-        user_form = UserRegistrationForm()
-    return  render(request, 'account/register.html', {'user_form': user_form})
+        user_form = UserRegistrationForm2()
+    return render(request, 'account/register.html', {'user_form': user_form})
 
 
 
